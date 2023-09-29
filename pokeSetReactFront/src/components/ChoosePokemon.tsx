0aa35ee12f0544ec012e3f84457f72c844daa7c0
toddcmz <toddcmz@gmail.com>
2023-09-29 15:41:04 -0600

    split sprite retrieval functions to accommodate picking your own or surprise me, got surprise me button working as expected

--- pokeSetReactFront/src/components/ChoosePokemon.tsx
+++ pokeSetReactFront/src/components/ChoosePokemon.tsx
@@ -17,6 +17,33 @@ export default function ChoosePokemon() {
         const pmon2Name = pmon2Field.current?.value
         const pmon3Name = pmon3Field.current?.value
         
+        await retrieveSprites(pmon1Name, pmon2Name, pmon3Name)
+    }
+
+    async function handleSurpriseMeButton(){
+        const pokeList:string[] = allPokemonList
+
+        let num1 = Math.floor(Math.random() * (386 - 0))
+        let num2 = num1
+        while (num2 === num1){
+            num2 = Math.floor(Math.random() * (386 - 0))
+        }
+        let num3 = num1
+        while (num3 === num1 || num3 === num2){
+            num3 = Math.floor(Math.random() * (386 - 0))
+        }
+
+        const pmon1Name = pokeList[num1]
+        const pmon2Name = pokeList[num2]
+        const pmon3Name = pokeList[num3]
+
+        await retrieveSprites(pmon1Name, pmon2Name, pmon3Name)
+        console.log(pmon1Name, pmon2Name, pmon3Name)
+    }
+
+    async function retrieveSprites(pmon1Name:string|undefined, pmon2Name:string|undefined, pmon3Name:string|undefined){
+
+        setPmonImages([])
 
         const res1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pmon1Name}`)
         const res2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${pmon2Name}`)
@@ -39,38 +66,23 @@ export default function ChoosePokemon() {
         }
     }
 
-    async function handleSurpriseMeButton(){
-        const pokeList:string[] = allPokemonList
-
-        let num1 = Math.floor(Math.random() * (386 - 0))
-        let num2 = num1
-        while (num2 === num1){
-            num2 = Math.floor(Math.random() * (386 - 0))
-        }
-        let num3 = num1
-        while (num3 === num1 || num3 === num2){
-            num3 = Math.floor(Math.random() * (386 - 0))
-        }
-
-        console.log(num1, num2, num3)
-        console.log(pokeList[0])
-    }
-
   return (
     <>
-
-        <div> 
+        
+        <div className="selectorDirectionsContainer"> 
             <h3>Choose Pokemon</h3>
             <h5>Warning: choose pkmn validation not yet implemented. Pkmn through emerald gen are available.</h5>
             <h5>Refresh page and choose new pkmn if not all cards display a sprite.</h5>
         </div>
+        <div className="pokemonSelectors">
         <form onSubmit={handleChoosePokemonForm}>
             <input type="text" placeholder="Pokemon 1" ref={pmon1Field}/>
             <input type="text" placeholder="Pokemon 2" ref={pmon2Field}/>
             <input type="text" placeholder="Pokemon 3" ref={pmon3Field}/>
-            <button>Catch 'em!</button>
+            <button className="pmonSelectorButtons">Catch 'em!</button>
         </form>
-        <button onClick={handleSurpriseMeButton}>Surprise Me</button>
+        <button className="pmonSelectorButtons" onClick={handleSurpriseMeButton}>Surprise Me</button>
+        </div>
         <GameBoard imgUrls={pmonImages}/>
     </>
   )
